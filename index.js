a41d034bb4292b3a9d77ddf0aaa6956408b7b59e
youtanimstar <animdeep2019@gmail.com>
2022-08-28 09:20:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const joke = document.querySelector('#joke');
+const jokeBtn = document.querySelector('#jokeBtn');
+
+const jenerateJokes = async() =>{
+    try{
+        const setHeader ={
+        headers:{
+            Accept: "application/json"
+        }
+    }
+    const res = await fetch('https://icanhazdadjoke.com/', setHeader);
+    const data = await res.json();
+    joke.innerHTML = await data.joke;
+}
+catch(err)
+{
+    joke.innerHTML = err;
+}
+        // .then((res) =>res.json())
+        // .then((data) =>{
+        //     joke.innerHTML = data.joke;
+        // })
+        // .catch((err)=>{
+        //     joke.innerHTML = err;
+        // })
+}
+
+jokeBtn.addEventListener('click', jenerateJokes);
+jenerateJokes();
